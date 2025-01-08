fdec1c1c6c722f1d28c51b4e2762621236d12242
Oluwayemisi <anuoluwayemisi@gmail.com>
2025-01-08 17:58:50 +0100

    payment method integration

--- orders/models.py
+++ orders/models.py
@@ -3,10 +3,32 @@ from django.conf import settings
 from products.models import Product
 
 
+class Payment(models.Model):
+    PAYMENT_STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Failed', 'Failed'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=PAYMENT_STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    payment_id = models.CharField(max_length=100, blank=True, null=True)
+
+
 class Order(models.Model):
+    ORDER_STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Canceled', 'Canceled'),
+    ]
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default='Pending')
+    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
@@ -20,4 +42,3 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.product.name} {self.quantity} {self.price}"
-
